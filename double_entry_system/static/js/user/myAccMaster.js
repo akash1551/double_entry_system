cc6b231b8761aaf41b874bc93eaff69ffa15877e
Mohit Patil <mohitpatil546@gmail.com>
2016-02-03 12:25:10 +0530

    myAccMaster template data is displayed

--- double_entry_system/static/js/user/myAccMaster.js
+++ double_entry_system/static/js/user/myAccMaster.js
@@ -3,13 +3,17 @@ new Vue ({
 	el: '#myAccMaster',
 
 	data: {
-		message: 'hellllo world'
+		accYearsList: []
 	},
 
 	ready: function() {
 		this.$http.get('/list_of_accounting_years/').then(function (response){
 			console.log(response.data);
-
+			if(!response.data.status){
+				console.log('Error');
+			}else{
+				this.accYearsList = response.data.AccYearsList;
+			}
 		}, function (response){
 			console.log(response.data);
 		});
