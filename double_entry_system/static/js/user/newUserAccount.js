c80aa6f8ee5e23999ecb72c3d597a82e11ee9db1
Mohit Patil <mohitpatil546@gmail.com>
2016-02-03 14:21:57 +0530

    newUserAccount template vue is added

--- double_entry_system/static/js/user/newUserAccount.js
+++ double_entry_system/static/js/user/newUserAccount.js
@@ -2,8 +2,7 @@ new Vue ({
 	el: '#newUserAccount',
 
 	data: {
-		newUser: {
-			userName: '',
+			accountName: '',
 			alias: '',
 			firstName: '',
 			lastName: '',
@@ -17,13 +16,59 @@ new Vue ({
 			mobileNo0: '',
 			mobileNo1: '',
 			openingBalance: '',
-			groupName: ''
-		}
+			group: '',
+			accountType: '',
+			duration: 1,
+			startDate: '',
+			endDate: '',
+
+			groupList: [],
+			accountTypeList: []
+	},
+
+	ready: function(){
+		this.$http.get('/get_groups_from_db/').then(function (response){
+			console.log(response.data);
+			if(!response.data.status){
+				console.log('error');
+			}else{
+				this.groupList = response.data.accGroupList;
+			}
+		}, function (response){
+			console.log(response.data);
+		});
+
+
+		this.$http.get('/get_accounttype_from_db/').then(function (response){
+			console.log(response.data);
+			if(!response.data.status){
+				console.log('Error');
+			}else{
+				this.accountTypeList = response.data.accTypeList;
+			}
+		}, function (response){
+			console.log(response.data);
+		});
 	},
 
 	methods: {
 		createNewUser: function(){
 			console.log(this.newUser);
+
+			this.$http.post('/create_new_user_account/', {
+				account_name: this.accountName, alias : this.alias,
+				firstName: this.firstName, lastName: this.lastName,
+				addressLine1: this.addressLine1, addressLine2: this.addressLine2,
+				city: this.city, state: this.state, country: this.country, pincode: this.pincode,
+				email: this.email, mobileNo0: this.mobileNo0, mobileNo1: this.mobileNo1,
+				group: this.group, openingBalance: this.openingBalance, accounttype: this.accountType,
+				duration: this.duration, start_date: this.startDate, end_date: this.endDate
+
+			}).then(function (response){
+				console.log(response.data);
+			}, function (response){
+				console.log(response.data);
+			});
 		}
 	}
 });
