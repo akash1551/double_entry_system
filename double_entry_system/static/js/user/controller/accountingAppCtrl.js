e9412239b63b20078e78015cea3d2014cc8f5658
Mohit Patil <mohitpatil546@gmail.com>
2016-02-05 16:57:46 +0530

    accountingApp template is modified

--- double_entry_system/static/js/user/controller/accountingAppCtrl.js
+++ double_entry_system/static/js/user/controller/accountingAppCtrl.js
@@ -96,12 +96,14 @@ angular.module('userApp.controllers')
 
 	$scope.addEntry = function(){
 		if(tranTypeValidation($scope.tranType)){
-			if($scope.tranType == 'C' && $scope.credit != null){
+			if($scope.tranType == 'C' && $scope.credit != null && $scope.account != null){
 				$scope.tranList.push({is_debit: $scope.tranType, amount: $scope.credit, account: $scope.account});
 				$scope.credit = null;
-			}else if($scope.tranType == 'D' && $scope.debit != null){
+			}else if($scope.tranType == 'D' && $scope.debit != null && $scope.account != null){
 				$scope.tranList.push({is_debit: $scope.tranType, amount: $scope.debit, account: $scope.account});
 				$scope.debit = null;
+			}else{
+				Notification.error({message: 'Plase Fill all details'});
 			}
 			$('#tranType').focus();
 			console.log($scope.tranList);
@@ -121,9 +123,32 @@ angular.module('userApp.controllers')
 			description: $scope.description,
 			transactiontype: $scope.transactionMode.id
 		};
-		var dataPromis = networkService.saveTransactionEntryRequest(obj);
-		dataPromis.then(function(result){
-			console.log(result);
-		});
+
+		if(obj.transaction_date != null && obj.transactiontype != {} && obj.description != ''){
+			if(validateTransactions($scope.tranList) == 0){
+				var dataPromis = networkService.saveTransactionEntryRequest(obj);
+				dataPromis.then(function(result){
+					console.log(result);
+				});
+			}else{
+				Notification.error({message: "You credit and Debit entries amount doesn't match"});
+			}
+		}else{
+			Notification.error({message: "Please Fill all mandatory fields"});
+		}
+	};
+
+	var validateTransactions = function(list){
+		var cr = 0,
+			dr = 0;
+		for(var i = 0; i < $scope.tranList.length; i++){
+			if($scope.tranList[i].is_debit == 'C'){
+				cr = cr + $scope.tranList[i].amount;
+			}else{
+				dr = dr + $scope.tranList[i].amount;
+			}
+		}
+
+		return cr-dr;
 	};
 });
