1256d61fbdf6ae7ebb2e501079b71b75a6e5e05b
Mohit Patil <mohitpatil546@gmail.com>
2016-02-05 11:42:42 +0530

    accountingApp template trasaction type field is added

--- double_entry_system/static/js/user/controller/accountingAppCtrl.js
+++ double_entry_system/static/js/user/controller/accountingAppCtrl.js
@@ -1,4 +1,16 @@
 angular.module('userApp.controllers')
 .controller('accountingAppController', function($scope){
 	console.log('accountingAppController is loaded');
+
+	$scope.tranList = [];
+
+	$scope.tranTypeFilter = function(val){
+		if(val == 'c' || val == 'C'){
+			return 'C';
+		}else if(val == 'd' || val == 'D'){
+			return 'D';
+		}else{
+			//
+		}
+	};
 });
