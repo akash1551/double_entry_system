b1c23e10a87088941c67e260ac4c736e17d9dbb1
Mohit Patil <mohitpatil546@gmail.com>
2016-02-05 11:59:53 +0530

    accountingApp template add and remove entry functionaliity is added

--- double_entry_system/static/js/user/controller/accountingAppCtrl.js
+++ double_entry_system/static/js/user/controller/accountingAppCtrl.js
@@ -3,14 +3,35 @@ angular.module('userApp.controllers')
 	console.log('accountingAppController is loaded');
 
 	$scope.tranList = [];
+	$scope.tranType = 'C';
+	$scope.inputTabs = false;
+	$scope.credit = null;
+	$scope.debit = null;
 
 	$scope.tranTypeFilter = function(val){
 		if(val == 'c' || val == 'C'){
+			$scope.inputTabs = false;
 			return 'C';
 		}else if(val == 'd' || val == 'D'){
+			$scope.inputTabs = true;
 			return 'D';
 		}else{
 			//
 		}
+	};
+
+	$scope.addEntry = function(){
+		if($scope.tranType == 'C' && $scope.credit != null){
+			$scope.tranList.push({is_debit: $scope.tranType, amount: $scope.credit});
+			$scope.credit = null;
+		}else if($scope.tranType == 'D' && $scope.debit != null){
+			$scope.tranList.push({is_debit: $scope.tranType, amount: $scope.debit});
+			$scope.debit = null;
+		}
+		console.log($scope.tranList);
+	};
+
+	$scope.removeEntry = function(index){
+		$scope.tranList.splice(index, 1);
 	};
 });
