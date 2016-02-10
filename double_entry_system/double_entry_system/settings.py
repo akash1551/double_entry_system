0654faf8dcc4840a76016fead32a6d2c06cfd6a9
Akash <acwankhede@gmail.com>
2016-02-10 18:00:52 +0530

    added settings file with changing database unlock timeout value

--- double_entry_system/double_entry_system/settings.py
+++ double_entry_system/double_entry_system/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'DATABASE_OPTIONS':{'timeout': 05}
+        'DATABASE_OPTIONS':{'timeout': 07}
     }
 }
 
